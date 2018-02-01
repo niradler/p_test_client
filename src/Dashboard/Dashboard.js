88c1cfb307734807a71bc3af8fa46baffc3d679f
Nir <niradler55@gmail.com>
2018-02-01 21:23:49 +0200

    add multi select filter

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
-import {fetchData,getFields} from '../util'
+import {fetchData, getFields} from '../util'
 import logo from '../logo.png';
+import Header from '../components/Header/Header'
 import Filter from '../components/Filter/Filter'
 import Table from '../components/Table/Table'
+import Summary from '../components/Summary/Summary'
 import './Dashboard.css';
 
 class Dashboard extends Component {
@@ -13,45 +15,52 @@ class Dashboard extends Component {
     }
   }
   componentWillMount() {
-    if (this.state.data.length ===0) {
+    if (this.state.data.length === 0) {
       fetchData().then((res) => {
         this.setState({
-          fields:getFields(res),
+          fields: getFields(res),
           data: res
-        },()=>console.log("data",this.state))
+        }, () => console.log("data", this.state))
       })
     }
 
   }
-apply = (filter)=>{
-  fetchData(filter).then((res) => {
-    this.setState({
-      fields:[],
-      data: []
-    },()=>{
+  apply = (filter) => {
+    fetchData(filter).then((res) => {
       this.setState({
-        fields:getFields(res),
-        data: res
+        fields: [],
+        data: []
+      }, () => {
+        this.setState({fields: getFields(res), data: res})
       })
     })
-  })
-}
+  }
   render() {
     return (
-      <div className="Dashboard container">
-        {/* <header >
-          <img src={logo} className="logo" alt="logo" />
-        </header> */}
-        {this.state.data.length > 0 ?
-        <Filter fields={this.state.fields} apply={this.apply}/>
-        :"Loading..."}
-        {this.state.data.length > 0 ?
-        <Table 
-        table_head={['ip','date','domain','blacklisted','event type']}
-        data={this.state.data}
-        />
-        :"Loading..."}
+      <div className="Dashboard ">
+        <Header/>
+        <div className="container">
+          {this.state.data.length > 0
+            ? 
+            <div>
+              <Summary reports={this.state.data.length} blacklisted={this.state.data.filter((e)=> e.blacklisted).length}/>
+            <Filter fields={this.state.fields} apply={this.apply}/>
+            <Table
+                table_head={['ip', 'date', 'domain', 'blacklisted', 'event type']}
+                data={this.state.data}/>
+                </div>
+            : <div className="modal is-active">
+              <div className="modal-background"></div>
+              <div className="">
+                <div className="loading">
+                  <img src={logo} className="logo" alt="logo"/>
+                </div>
 
+              </div>
+              <button className="modal-close is-large" aria-label="close"></button>
+            </div>
+}
+        </div>
       </div>
     );
   }
--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 class Filter extends Component {
   constructor(props) {
     super();
     this.state = {
+      show:false,
       form:{},
       fields:{
         ips_options:[],
@@ -22,72 +25,102 @@ class Filter extends Component {
   }
   onChange = (evt)=>{
     const state = this.state;
-    state.form[evt.target.name] = evt.target.value
+    if (evt !==null && evt.target !== undefined) {
+      state.form[evt.target.name] = evt.target.value
+      this.setState(state)
+    }
+  }
+  handleSelect =(field,data)=>{
+    const state = this.state;
+    if(field  === 'blacklisted') data.value = (data.value === 'true');
+    if(typeof data === 'object' && data.length) state.form[field] = data;
+    else state.form[field] = data.value;
+    
     this.setState(state)
   }
+reset = ()=>{
+  this.props.apply();
+}
   apply = (e)=>{
     e.preventDefault();
-    this.props.apply(this.state.form)
+    let form = this.state.form
+    for (const key in form) {
+      if(form[key]==='none'){
+        delete form[key];
+      }
+    }
+    if (form.domain) form.domain = form.domain.map((d)=>d.value)
+ 
+    this.props.apply(form)
   }
   render() {
     return (
       <div id="Filter">
+      <div class="card">
+        {this.state.show ?
+  <div class="card-content">
+
         <div className="columns">
-          <div className="column is-2">
+          <div className="column is-3">
             <div className="field">
               <label className="label className">IP:</label>
               <div className="control has-icons-left has-icons-right">
-              <div className="select">
-              <select onChange={this.onChange} name="ip">
-             {this.state.fields.ips_options.map((ip)=><option key={ip} value={ip}>{ip}</option>) }
-              </select>
-            </div>
+              <Select
+                name="ip"
+                value={this.state.form.ip}
+                onChange={(data)=>this.handleSelect('ip',data)}
+                options={this.state.fields.ips_options.map((i)=> {return { value: i, label: i }})}
+              />
               </div>
             </div>
           </div>
  
-          <div className="column is-1">
+          <div className="column is-2">
             <div className="field">
               <label className="label className">Blacklisted:</label>
               <div className="control has-icons-left has-icons-right">
-              <div className="select">
-              <select onChange={this.onChange} name="blacklisted">
-              {this.state.fields.blacklisted.map((b)=><option key={b} value={b}>{b}</option>) }
-              </select>
-            </div>
+              <Select
+              name="blacklisted"
+              value={this.state.form.blacklisted}
+              onChange={(data)=>this.handleSelect('blacklisted',data)}
+              options={this.state.fields.blacklisted.map((i)=> {return { value: i, label: i }})}
+            />
               </div>
             </div>
           </div>
-          <div className="column is-2">
+          <div className="column is-3">
             <div className="field">
               <label className="label className">Event type:</label>
               <div className="control has-icons-left has-icons-right">
-              <div className="select">
-              <select onChange={this.onChange} name="event_type">
-              {this.state.fields.events_options.map((e)=><option key={e} value={e}>{e}</option>) }
-              </select>
-            </div>
+              <Select
+              name="event_type"
+              value={this.state.form.event_type}
+              onChange={(data)=>this.handleSelect('event_type',data)}
+              options={this.state.fields.events_options.map((i)=> {return { value: i, label: i }})}
+            />
               </div>
             </div>
           </div>
        
           </div>
           <div className="columns">
-          <div className="column is-4">
+          <div className="column is-6">
             <div className="field">
               <label className="label className">Domain:</label>
               <div className="control has-icons-left has-icons-right">
-              <div className="">
-              <select multiple onChange={this.onChange} name="domain">
-              {this.state.fields.domains.map((d)=><option key={d} value={d}>{d}</option>) }
-              </select>
-            </div>
+              <Select
+              multi
+              name="domain"
+              value={this.state.form.domain}
+              onChange={(data)=>this.handleSelect('domain',data)}
+              options={this.state.fields.domains.map((ip)=> {return { value: ip, label: ip }})}
+            />
               </div>
             </div>
           </div>
           </div>
           <div className="columns">
-          <div className="column is-3">
+          <div className="column is-5">
             <div className="field">
               <label className="label className">from:</label>
               <div className="control has-icons-left has-icons-right">
@@ -99,7 +132,7 @@ class Filter extends Component {
               </div>
             </div>
           </div>
-          <div className="column is-3">
+          <div className="column is-5">
             <div className="field">
               <label className="label className">to:</label>
               <div className="control has-icons-left has-icons-right">
@@ -114,15 +147,28 @@ class Filter extends Component {
           </div>
          
           <div className="columns">
-          <div className="column">
-            <div className="field">
-              <label className="label className">Action:</label>
-              <div className="control has-icons-left has-icons-right">
-              <button className="button is-info" onClick={this.apply}>Apply</button>
-             </div>
-            </div>
-          </div>
+          
+          <div className="column is-1">
+          <div className="control ">
+          <button className="button is-info" onClick={this.apply}>Apply</button>
+         </div>
+         </div>
+         <div className="column is-1">
+          <div className="control ">
+          <button className="button is-danger" onClick={this.reset}>Reset</button>
+         </div>
+         </div>
+         <div className="column is-1">
+         <div className="control ">
+          <button className="button is-warning" onClick={()=>this.setState({show:false})}>Hide</button>
+         </div>
         </div>
+
+        </div>
+
+        </div>
+        : <button className="button is-link is-outlined is-fullwidth" onClick={()=>this.setState({show:true})}>Show Filter</button>}
+</div>
       </div>
     );
   }
